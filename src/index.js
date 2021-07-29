6ff527f9a11d70c38b2b10ff45a209cdf5157d3e
maahisonti218 <mahisonti218@gmail.com>
2021-07-29 13:57:31 +0530

    session, logout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express1 = require("express");
-const connection = require("./connection/Connection");
+const connection1 = require("./connection/Connection");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
+const session = require("express-session");
+const session_secret = "ProMentorTechs";
 const app = express1();
 const PORT = 8081;
 const SALT = 10;
 const DB_PORT = 27017;
 const DB_NAME = "ProMentorTechs";
-const con = connection.getConnection({ port: DB_PORT, dbname: DB_NAME });
+const con = connection1.getConnection({ port: DB_PORT, dbname: DB_NAME });
 app.use(express1.json());
 mongoose.set("useFindAndModify", false);
 //schemas....
@@ -32,15 +35,32 @@ const loginModel = con.model("Login", loginSchema);
 const otpModel = con.model("otp", otpSchema);
 const testModel = con.model("test", testSchema);
 
-const Auth = (req, res, next) => {
-   if (req.query.pass === "pass123") {
-      console.log("if runs");
-      next();
+app.use(
+   cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+   })
+);
+app.use(
+   session({
+      secret: session_secret,
+      cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+   })
+);
+const AuthMiddleware = async (req, res, next) => {
+   if (
+      isNullOrUndefined(req.session) ||
+      isNullOrUndefined(req.session.userId)
+   ) {
+      res.status(401).send({ err: "not logged in" });
    } else {
-      console.log("else runs");
-      res.status(401).send({ err: "password not correct" });
+      next();
    }
 };
+app.get("/userinfo", AuthMiddleware, async (req, res) => {
+   const user = await loginModel.findById(req.session.userId);
+   res.send({ email: user.USEREMAIL });
+});
 app.post("/test", async (req, res) => {
    const { otp, email } = req.body;
    var currentTime = new Date();
@@ -63,6 +83,7 @@ app.post("/signup", async (req, res) => {
    const { username, email, password } = req.body;
    console.log(username, email, password);
    const exinstingUser = await loginModel.findOne({ EMAILID: email });
+   console.log(exinstingUser, "this is existing user");
    if (isNullOrUndefined(exinstingUser)) {
       const encryptpassword = bcrypt.hashSync(password, SALT);
       console.log(encryptpassword);
@@ -75,6 +96,8 @@ app.post("/signup", async (req, res) => {
       await user.save(userDetail).catch((e) => {
          res.status(400).send({ err: "failed :" + e });
       });
+      req.session.userId = user._id;
+
       res.status(201).send({ msg: "success" });
    } else {
       res.status(400).send({ err: `email already exist` });
@@ -88,18 +111,21 @@ app.post("/signin", async (req, res) => {
    console.log("url hit");
    try {
       const { email, password } = req.body;
-      const exinstingUser = await loginModel.findOne({ EMAILID: email });
-      if (isNullOrUndefined(exinstingUser)) {
-         res.status(401).send({ err:```email does not exit` });
+      const existingUser = await loginModel.findOne({ EMAILID: email });
+      console.log(existingUser, "abc");
+      if (isNullOrUndefined(existingUser)) {
+         res.status(401).send({ err: `Invalid usename/password` });
       } else {
-         const hashedPwd = exinstingUser.USERPASSWORD;
+         const hashedPwd = existingUser.USERPASSWORD;
          if (bcrypt.compareSync(password, hashedPwd)) {
+            req.session.userId = existingUser._id;
+
             res.status(200).send({
-               success: `log in`,
+               msg: `log in successfully`,
             });
          } else {
             res.status(401).send({
-               err: `password don't match`,
+               err: `Invalid usename/password`,
             });
          }
       }
@@ -107,6 +133,16 @@ app.post("/signin", async (req, res) => {
       res.status(500).send({
          err: `${e}`,
       });
+   }
+});
+app.get("/logout", async (req, res) => {
+   console.log("logout called");
+   if (!isNullOrUndefined(req.session)) {
+      req.session.destroy(() => {
+         res.sendStatus(200);
+      });
+   } else {
+      res.sendStatus(200);
    }
 });
 function generatOtp() {
@@ -120,10 +156,10 @@ function TimeDiff(startDate, endDate) {
    console.log(sec);
    return sec;
 }
-app.post("/forgotpass", async (req, res) => {
+app.post("/forgotpass", AuthMiddleware, async (req, res) => {
    const { type } = req.query;
    if (type === "email") {
-      const {email } = req.body;
+      const { email } = req.body;
       // console.log("user: ", email);
       const allData = await loginModel.find(
          { EMAILID: email },
@@ -136,21 +172,21 @@ app.post("/forgotpass", async (req, res) => {
          const name = allData[0].USERNAME;
          let otp = generatOtp();
          let result = await otpModel.findOneAndUpdate(
-            { "USEREMAIL": email },
-            { "OTP": otp, "DATETIME": time }
+            { USEREMAIL: email },
+            { OTP: otp, DATETIME: time }
          );
          console.log(result);
          if (result == null) {
-            console.log("iff null")
+            console.log("iff null");
             const otpDetail = {
                OTP: otp,
                USEREMAIL: email,
                DATETIME: time,
             };
-      
+
             const result3 = new otpModel(otpDetail);
             result = await result3.save();
-}
+         }
          const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 587,
@@ -181,59 +217,65 @@ app.post("/forgotpass", async (req, res) => {
 
               ${timeString}
                `,
-             };
+         };
 
-             transporter.sendMail(mailOptions, function (error, info) {
+         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                console.log(error);
-               res.status(400).send("mail not sent");
+               res.status(400).send({ err: "mail not sent" });
             } else {
                console.log("Email sent: " + info.response);
-               res.status(200).send("mail sent");
-               console.log("here");
+               res.status(200).send({ msg: "mail sent" });
             }
          });
       } else {
-         res.status(400).send("Invalid email id");
+         res.status(400).send({ err: "Invalid email id" });
       }
    }
    //type== email
-   if(type==="otp"){
-      const {otp,email}=req.body;
-      const existEmail = await otpModel.findOne({USEREMAIL:email},{OTP:1,DATETIME:1,"_id":0 });
-console.log("this is nagina ki line",existEmail);
-const currTime=new Date();
-if(existEmail!==null){
-   console.log(existEmail.OTP,"otp line")
-   const sec=TimeDiff(currTime,new Date(existEmail.DATETIME));
-
-   if(existEmail.OTP==otp&&sec<180){
-      console.log(sec,"seconds")
-      res.status(200).send({msg:"success"});
-   }else{
-      res.status(200).send({msg:"otp doesn't match"});
+   if (type === "otp") {
+      const { otp, email } = req.body;
+      const existEmail = await otpModel.findOne(
+         { USEREMAIL: email },
+         { OTP: 1, DATETIME: 1, _id: 0 }
+      );
+      console.log("this is nagina ki line", existEmail);
+      const currTime = new Date();
+      if (existEmail !== null) {
+         console.log(existEmail.OTP, "otp line");
+         const sec = TimeDiff(currTime, new Date(existEmail.DATETIME));
+         console.log("seconds out:", sec);
+         console.log(existEmail.OTP);
+         console.log(otp);
+         if (existEmail.OTP == otp && sec < 180) {
+            console.log(sec, "seconds");
+            res.status(200).send({ msg: "success" });
+         } else {
+            res.status(200).send({ err: "otp doesn't match" });
+         }
+      } else {
+         res.status(400).send({ err: "error" });
+      }
    }
-}else{
-   res.status(400).send({err:"error"});
-
-}
+   if (type === "confirmpass") {
+      const { email, password } = req.body;
+      console.log("last password", email, password);
+      if (password !== null && password !== "" && password !== undefined) {
+         const encryptpassword = bcrypt.hashSync(password, SALT);
+         console.log(encryptpassword);
+         const existEmail = await loginModel.findOneAndUpdate(
+            { EMAILID: email },
+            { USERPASSWORD: encryptpassword }
+         );
+         if (existEmail !== null) {
+            res.status(200).send({ msg: "success" });
+         } else {
+            res.status(400).send({ err: "email doesn't exist" });
+         }
+      } else {
+         res.status(400).send({ err: "Invalid password" });
+      }
    }
-   if(type==="confirmpass"){
-const {email,password}=req.body;
-if(password!==null&&password!==""&&password!==undefined){
-   const encryptpassword = bcrypt.hashSync(password, SALT);
-   console.log(encryptpassword);
-   const existEmail = await loginModel.findOneAndUpdate({EMAILID:email},{"USERPASSWORD":encryptpassword});
-   if(existEmail!==null){
-res.status(200).send({msg:"success"});
-   }else{
-      res.status(400).send({err:"email doesn't exist"});
-   }
-}else{
-   res.status(400).send({err:"Invalid password"});
-
-}
-}
 });
 
 app.listen(PORT, () => {
